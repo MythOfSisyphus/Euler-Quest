8344de380790203610b4f7d73d59cdd32ea1c5c8
Lucky <luckychouhan369@gmail.com>
2025-11-09 00:06:01 +0530

    A file to populate archives.html page.

---
+++ js/archives-data.js
@@ -0,0 +1,10 @@
+const archivesData = [
+    { day: 1, title: "Largest Product in a Series", problem: 8 },
+    { day: 2, title: "Longest Collatz Sequence", problem: 14 },
+    { day: 3, title: "Lattice Paths", problem: 15 },
+    { day: 4, title: "Power Digit Sum", problem: 16 },
+    { day: 5, title: "Number Letter Counts", problem: 17 },
+    { day: 6, title: "Maximum Path Sum", problem: 18 },
+    { day: 7, title: "Counting Sundays", problem: 19 },
+    { day: 8, title: "Non-Abundant Sums", problem: 23 },
+];
